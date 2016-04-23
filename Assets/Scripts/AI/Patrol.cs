2648af853c243554b5cbef7ebd4b7d08c5cfc1e6
tiagommferreira <tiagommferreira55@gmail.com>
2016-04-23 16:16:42 +0100

    ncp moving

---
+++ Assets/Scripts/AI/Patrol.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+        anim.SetBool("Grounded", true);
+        anim.SetBool("Aim", false);
+        anim.SetFloat("Speed", agent.speed, 0.1f, Time.deltaTime);
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
