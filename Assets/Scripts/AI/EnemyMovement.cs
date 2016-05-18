40e3cdbe1568d1473ef420bbe1e34fd35b801fef
Rui Gomes <hello@ruigomes.me>
2016-05-18 16:57:12 +0100

    New texture

--- Assets/Scripts/AI/EnemyMovement.cs
+++ Assets/Scripts/AI/EnemyMovement.cs
@@ -9,6 +9,8 @@ public class EnemyMovement : MonoBehaviour
     NavMeshAgent nav;                 // Reference to the nav mesh agent.
     public float minimum_distance = 50f;
 
+	private int movingBool;
+	private Animator animator;
 
     void Awake()
     {
@@ -17,22 +19,32 @@ public class EnemyMovement : MonoBehaviour
         //playerHealth = player.GetComponent<PlayerHealth>();
         //enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator> ();
+		movingBool = Animator.StringToHash ("isMoving");
     }
 
 
     void Update()
     {
+		float currentDistance = Vector3.Distance (nav.transform.position, player.transform.position);
+
+		Debug.Log (currentDistance);
         // If the enemy is close enough
-        if (Vector3.Distance(nav.transform.position, player.transform.position) < minimum_distance)
+		if (currentDistance <= 140/100) {
+			animator.SetBool (movingBool, false);
+		}
+		else if (currentDistance < minimum_distance)
         {
             // ... set the destination of the nav mesh agent to the player.
             nav.SetDestination(player.position);
+			animator.SetBool (movingBool, true);
         }
         else
         {
             // ... disable the nav mesh agent.
             //nav.enabled = false;
-            nav.SetDestination(nav.transform.position);
+			nav.SetDestination(nav.transform.position);
+			animator.SetBool (movingBool, false);
         }
     }
 } 
