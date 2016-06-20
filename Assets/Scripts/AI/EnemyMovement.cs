3916f86d9f9e09030b67a696bd4463388a2ee208
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2016-06-20 10:40:37 +0100

    disable instead of destroy and dead animations in aliens

--- Assets/Scripts/AI/EnemyMovement.cs
+++ Assets/Scripts/AI/EnemyMovement.cs
@@ -11,8 +11,9 @@ public class EnemyMovement : MonoBehaviour
 
 	private int movingBool;
 	private int attackingBool;
+	private int deadBool;
+	private double dead=0f;
 	private Animator animator;
-
 	private float lastStopAnimation;
 
 
@@ -25,6 +26,7 @@ public class EnemyMovement : MonoBehaviour
 		animator = GetComponent<Animator> ();
 		movingBool = Animator.StringToHash ("isMoving");
 		attackingBool = Animator.StringToHash ("isAttacking");
+		deadBool = Animator.StringToHash ("isDead");
 
     }
 
@@ -65,13 +67,19 @@ public class EnemyMovement : MonoBehaviour
 			nav.SetDestination(nav.transform.position);
 		}
 
+		if (dead != 0 && dead + 2f < Time.time) {
+			gameObject.SetActive (false);
+		}
+
 	}
 
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.gameObject.tag == "ThrowingRock")
 		{
-			Destroy(gameObject);
+			animator.SetBool (deadBool, true);
+			Debug.Log ("entrou na colisao da pedra");
+			dead = Time.time;
 		}
 	}
 
